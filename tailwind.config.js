bd73fbcfd294bf59fa138b5493f41dd917b7ce3f
kaito.umemoto <kaito.umemoto@uhd-inc.jp>
2025-11-24 17:41:00 +0900

    ヘッダー作成

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        catchy: ['"Catchy Mager"', 'serif'],
+      },
+    },
+  },
+};
+
