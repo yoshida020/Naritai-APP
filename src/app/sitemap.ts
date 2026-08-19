7be9381996aadeda25cf56190e44e6027a164f40
yoshida020 <yoshida.asuka020@gmail.com>
2026-08-19 19:08:57 +0900

    seo対策

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -17,6 +17,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${baseUrl}/lp`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    },
+    {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
